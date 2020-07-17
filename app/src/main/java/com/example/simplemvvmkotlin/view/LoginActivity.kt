e1cf6b1991c16ec650d40b6826f2f884ded1fc3d
Guilherme <gui.mergulhao55@outlook.com>
2020-07-17 09:16:11 -0300

    Tela de login consertada

--- app/src/main/java/com/example/simplemvvmkotlin/view/LoginActivity.kt
+++ app/src/main/java/com/example/simplemvvmkotlin/view/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,28 +16,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var viewModel : MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        viewModel.getTheRepositoryUsers().observe(this, object: Observer<ArrayList<User>>{
-            override fun onChanged(t: ArrayList<User>?) {
+    }
 
-                btnEnter.setOnClickListener {
-                    for (i in t!!) {
-                        if (edtName.text.toString().equals(i.name) && edtPassword.text.toString().equals(i.phrase)) {
-                            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                        } else {
-                            Toast.makeText(this@LoginActivity,"usuario não existe",Toast.LENGTH_SHORT).show()
-                        }
-                    }
+    fun loginProcess(view: View) {
 
-                }
-            }
+        if(viewModel.isThereThisUser(edtName.text.toString(), edtPassword.text.toString())){
 
-        })
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+
+        } else {
+
+            Toast.makeText(this@LoginActivity,"usuario não existe",Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 }
--- app/src/main/java/com/example/simplemvvmkotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/simplemvvmkotlin/viewmodel/MainViewModel.kt
@@ -7,9 +7,30 @@ import com.example.simplemvvmkotlin.repository.UserRepository
 
 class MainViewModel : ViewModel() {
 
+    var repositoryUsers = UserRepository.instance.getTheUsers()
+
     fun getTheRepositoryUsers() : MutableLiveData<ArrayList<User>>{
 
-        return UserRepository.instance.getTheUsers()
+        return repositoryUsers
+
+    }
+
+    fun isThereThisUser(name: String, pass: String) : Boolean{
+
+        var isThere = false
+
+        for(i in repositoryUsers.value!!){
+
+            if(i.name==name && i.phrase==pass){
+
+                isThere = true
+                break
+
+            }
+
+        }
+
+        return isThere
 
     }
 
